807c238a3eb989c6eb8b7d4938cb4166c6cb2b9c
ETTABTI-Hajar <hajarettabti2003@gmail.com>
2026-06-09 18:40:41 +0100

    logo

--- lib/cycle1/views/education/phase_education_screen.dart
+++ lib/cycle1/views/education/phase_education_screen.dart
@@ -2,9 +2,7 @@ import 'package:zyra/cycle1/data/models/cycle_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../../core/constants/app_colors.dart';
 import '../../core/constants/app_dimensions.dart';
-import '../../core/constants/app_text_styles.dart';
 import '../../viewmodels/education_viewmodel.dart';
 import '../shared_header.dart';
 import 'widgets/phase_tab_bar_widget.dart';
